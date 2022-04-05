95e04ae90c13e25d8554e11cd1d24c27a27647be
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-04-05 21:57:27 +0300

    update

--- info/models.py
+++ info/models.py
@@ -262,6 +262,9 @@ class AttendanceRange(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
 
+    def __str__(self):
+        return self.start_date
+
 
 # Triggers
 
